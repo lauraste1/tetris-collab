ffc97616b6f68649891146f5d82c8fee9962f2f3
Matt <schroedingers.hat@gmail.com>
2020-06-03 12:35:44 +0000

    Multiple directory makefile. Tidied tests

--- test/test_block.cpp
+++ test/test_block.cpp
@@ -9,34 +9,38 @@ using namespace std;
 int test_ct = 0;
 int pass_ct = 0;
 
-void expect(bool test, const string message);
+bool expect(bool test, const string message);
 
 int main() {
 
     Block block = Block(Block::straight);
-    expect(block.getX() == 0 &&  block.getY() == 0, "Initial position.");
+    expect(block.getX() == 0 &&  block.getY() == 0, "Initial position. ");
 
     block.shift(0,1);
-    expect(block.getX() == 0 &&  block.getY() == 1, "Shift.");
+    expect(block.getX() == 0 &&  block.getY() == 1, "Shift. ");
     string test_string_1 = " +  \n"\
                            " +  \n"\
                            " +  \n"\
                            " +  ";
 
-    expect(block.toString() == test_string_1, "To string."); 
+    if (!expect(block.toString() == test_string_1, "To string. ")) {
+        cout << "Block was: \n" << block.toString() << '\n';
+        cout << "Expected: \n" << test_string_1 << '\n';
+    }
     
 
-    cout << pass_ct << "/" << test_ct << "tests passed" << '\n';
+    cout << pass_ct << "/" << test_ct << " tests passed." << '\n';
 }
 
 
-void expect(bool test, const string message) {
+bool expect(bool test, const string message) {
     if (test) {
         cout << "Test " << ++test_ct << " passed." << '\n'; 
         ++pass_ct;
     }
     else {
-        cout << "Test " << ++test_ct << " failed." << '\n' << message << '\n';
+        cout << "Test " << ++test_ct << " failed: " << message << '\n';
     }
+    return test;
 }
 
