89559c33ed398f6b62a27912a5182a7dad976f09
Matthew Watson <matthew.watson@noisenet.com.au>
2020-06-09 15:35:12 +1000

    Game timer and update. Combine with other pull request and we get interactivity :D

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
 #include "display.hpp"
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
+
+// Just a way to have some state while gameboard is still not complete.
+struct DummyGameBoard {
+    int state;
+};
+
+// Gameboard or maybe user could be responsible for this?
+void advance_tick(Display *disp, SpriteSheet *sprites, DummyGameBoard *board) {
+    int i = board->state;
+    board->state += 1;
+    disp->blit(sprites->spriteSurf, &sprites->sprites[i%7], i % 10, (i / 10) % 21);
+}
 
 int main() {
     Display disp;
     SpriteSheet sprites("data/tetris.bmp");
     disp.draw_bg(sprites.spriteSurf, &sprites.sprites[7]);
+    DummyGameBoard board = {0};
 
-    for (int i=0;i<7;i++) {
-      disp.blit(sprites.spriteSurf, &sprites.sprites[i], i, 0);
+    auto now = chrono::steady_clock::now;
+    auto begin = now();
+
+    while ( disp.update() ) {
+      auto gameTime = chrono::duration_cast<milliseconds>(now() - begin);
+      while ( gameTime.count() / 500 > board.state )
+        // Also insert events at this point
+        //
+        // could even have a data structure for events
+        // so they are replayable.
+        advance_tick(&disp, &sprites, &board);
     }
-
-    bool quit = false;
-
-    while ( disp.update() );
     return 0;
 }
